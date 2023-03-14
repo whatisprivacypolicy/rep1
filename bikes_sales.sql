8879ef260f2b4e374347251e173918f8bfd009a1
whatisprivacypolicy <113383612+whatisprivacypolicy@users.noreply.github.com>
2023-03-14 19:24:45 +0300

    Add files via upload

--- bikes_sales.sql
+++ bikes_sales.sql
@@ -1,3 +1,5 @@
+use bike_stores;
+
 SELECT
 	ord.order_id,
 	concat(cus.first_name, ' ', cus.last_name) as customer,
@@ -7,6 +9,7 @@ SELECT
 	sum(ite.quantity) as total_units,
 	sum(ite.quantity * ite.list_price) as revenue,
 	pro.product_name,
+	bra.brand_name,
 	cat.category_name, 
 	sto.store_name,
 	concat(sta.first_name, ' ', sta.last_name) as sales_rep
@@ -17,6 +20,7 @@ JOIN production.products pro ON ite.product_id = pro.product_id
 JOIN production.categories cat ON pro.category_id = cat.category_id
 JOIN sales.stores sto ON ord.store_id = sto.store_id
 JOIN sales.staffs sta ON ord.staff_id = sta.staff_id
+JOIN production.brands bra ON pro.brand_id = bra.brand_id
 GROUP BY 
 	ord.order_id,
 	concat(cus.first_name, ' ', cus.last_name), 
@@ -24,6 +28,7 @@ GROUP BY
 	cus.state,
 	ord.order_date,
 	pro.product_name,
+	bra.brand_name,
 	cat.category_name,
 	sto.store_name,
 	concat(sta.first_name, ' ', sta.last_name)
